57108832ad975159ddcf6f88b33e9d8cd408c49c
Johannes Kraft <johannes.kraft1@gmail.com>
2024-07-06 12:44:25 +0200

    f

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
       "https://api.browse.ai/v2/robots/7dc791d6-407f-40a7-bb1c-44828319a52b/tasks/34b674b6-13f5-410c-b051-3437d9c27f44",
       {
         headers: {
-          Authorization: `Bearer ${process.env.BROWSEAI_TOKEN}`,
+          Authorization: `Bearer 7027a69b-202f-41ce-bc8b-4ddd7f5af1e0:290a3601-98c6-4ff1-b171-ad38a796cbd2`,
         },
       }
     )
@@ -18,6 +18,12 @@ app.get("/", (req, res) => {
         name: as.data.result.capturedTexts.name,
         concerts: as.data.result.capturedLists.concerts,
       };
+      const header = {
+        headers: {
+          Authorization: `Bearer ${process.env.BROWSEAI_TOKEN}`,
+        },
+      };
+      console.log("🚀 ~ app.get ~ header:", header);
       console.log(object);
       res.send(object);
     });
