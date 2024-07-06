b8b781c345ead9377e03b497cbf34d006b53fa29
Johannes Kraft <johannes.kraft1@gmail.com>
2024-07-06 12:56:37 +0200

    working auth

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 const app = express();
 const axios = require("axios").default;
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   axios
@@ -8,8 +9,7 @@ app.get("/", (req, res) => {
       "https://api.browse.ai/v2/robots/7dc791d6-407f-40a7-bb1c-44828319a52b/tasks/34b674b6-13f5-410c-b051-3437d9c27f44",
       {
         headers: {
-          Authorization:
-            "Bearer 7027a69b-202f-41ce-bc8b-4ddd7f5af1e0:290a3601-98c6-4ff1-b171-ad38a796cbd2",
+          Authorization: `Bearer ${process.env.BROWSEAI_TOKEN}`,
         },
       }
     )
@@ -18,7 +18,6 @@ app.get("/", (req, res) => {
         name: as.data.result.capturedTexts.name,
         concerts: as.data.result.capturedLists.concerts,
       };
-      console.log(object);
       res.send(object);
     });
 });
