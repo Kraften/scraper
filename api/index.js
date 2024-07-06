c7984c551ca51b8bfe8815de7b189c00d8e50fcc
Johannes Kraft <johannes.kraft1@gmail.com>
2024-07-06 12:45:38 +0200

    a

--- api/index.js
+++ api/index.js
@@ -9,7 +9,8 @@ app.get("/", (req, res) => {
       "https://api.browse.ai/v2/robots/7dc791d6-407f-40a7-bb1c-44828319a52b/tasks/34b674b6-13f5-410c-b051-3437d9c27f44",
       {
         headers: {
-          Authorization: `Bearer 7027a69b-202f-41ce-bc8b-4ddd7f5af1e0:290a3601-98c6-4ff1-b171-ad38a796cbd2`,
+          Authorization:
+            "Bearer 7027a69b-202f-41ce-bc8b-4ddd7f5af1e0:290a3601-98c6-4ff1-b171-ad38a796cbd2",
         },
       }
     )
