0ea9b4425512ea51f83e0982c86c6a69e27384ad
Johannes Kraft <johannes.kraft1@gmail.com>
2024-07-08 12:05:01 +0200

    q

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ initializeApp({
   credential: cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
+    private_key: process.env.PRIVATE_KEY.split(String.raw`\n`).join("\n"),
   }),
 });
 
